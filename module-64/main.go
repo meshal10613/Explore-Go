7be789e06b35467774b460260954bc6e91113c97
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-05-04 23:25:48 +0600

    feat: module 64.2- Variable Scope

--- module-64/main.go
+++ module-64/main.go
@@ -14,9 +14,14 @@ func main() {
 	// 	fmt.Printf("Making Coffee...")
 	// })()
 
-	(func(coffeeType string) {
-		fmt.Printf("Making %s Coffee...", coffeeType)
-	})("Black Coffee")
+	// (func(coffeeType string) {
+	// 	fmt.Printf("Making %s Coffee...", coffeeType)
+	// })("Black Coffee")
 
-
+	//? Variable Scope
+	x := 5
+	{
+		fmt.Printf("Inside the block: %d\n", x)
+	}
+	fmt.Printf("Outside the block: %d\n", x)
 }
