bd309d4a416a7c369bac3f00fb7b98bde11ef14c
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-05-05 19:07:19 +0600

    feat: module 65.2- Array in Go

---
+++ module-65/go.mod
@@ -0,0 +1,3 @@
+module module-65
+
+go 1.26.2
---
+++ module-65/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	//? Array in Go
+	// var num [6]int
+	// num[0] = 10 // start from 0 index
+	// fmt.Println("Array: ", num)
+	// fmt.Println("Length of Array: ", len(num))
+
+	// for i := 0; i < len(num); i++ {
+	// 	fmt.Printf("%d index is: %d\n", i, num[i])
+	// }
+
+	// numbers := [5]int{1, 2, 3, 4, 5}
+	// fmt.Println("Array:", numbers)
+}
