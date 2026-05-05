1d0943c05163f0a3007d3762c868bd2e8c482326
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-05-05 08:46:26 +0600

    feat: module 64.3- If, Else & Switch Statement

--- module-64/main.go
+++ module-64/main.go
@@ -19,9 +19,28 @@ func main() {
 	// })("Black Coffee")
 
 	//? Variable Scope
-	x := 5
-	{
-		fmt.Printf("Inside the block: %d\n", x)
+	// x := 5
+	// {
+	// 	fmt.Printf("Inside the block: %d\n", x)
+	// }
+	// fmt.Printf("Outside the block: %d\n", x)
+
+	//? If, Else & Switch Statement
+	num := 10
+	// if num%2 == 0 {
+	// 	fmt.Println("Even")
+	// } else if num%3 == 0 {
+	// 	fmt.Println("Divisible by 3")
+	// } else {
+	// 	fmt.Println("Odd")
+	// }
+
+	switch {
+	case num > 10:
+		fmt.Println("Greater than 10")
+	case num < 10:
+		fmt.Println("Less than 10")
+	default:
+		fmt.Println("Equal to 10")
 	}
-	fmt.Printf("Outside the block: %d\n", x)
 }
