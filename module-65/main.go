6361fa72657adf0fdf18e7f452d8679765d054c2
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-05-06 10:19:20 +0600

    feat: module 65.4- Slice in Go & length, capacity in slice

--- module-65/main.go
+++ module-65/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	//? Array in Go
 	// var num [6]int
@@ -13,4 +15,21 @@ func main() {
 
 	// numbers := [5]int{1, 2, 3, 4, 5}
 	// fmt.Println("Array:", numbers)
+
+	//? Slice in Go
+	orders := []int{10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
+	slice1 := orders[1:4] // slice from index 1 to 3 (4 is exclusive)
+	full := orders[:]     // slice of the entire array
+
+	fmt.Println("Slice:", slice1)
+	fmt.Println("Full:", full)
+
+	fmt.Println("Length of Slice:", len(slice1))
+	fmt.Println("Capacity of Slice:", cap(slice1))
+
+	slice1 = append(slice1, 500)
+	fmt.Println("Slice after append:", slice1)
+
+	fmt.Println("Length of Slice:", len(slice1))
+	fmt.Println("Capacity of Slice:", cap(slice1))
 }
