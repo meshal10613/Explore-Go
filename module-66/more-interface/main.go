abb57d627d78fcb89c92efcacc0be401943f7a72
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-05-09 19:18:39 +0600

    feat: mock payment method

--- module-66/more-interface/main.go
+++ module-66/more-interface/main.go
@@ -18,24 +18,46 @@ type Nagad struct {
 	apiKey string
 }
 
-func (bk Bkash) Pay(amount float64) {
-	fmt.Printf("Paying %.2f tk with Bkash", amount)
+func (bk *Bkash) Pay(amount float64) {
+	fmt.Printf("Paying %.2f tk with Bkash \n", amount)
 }
 
-func (ng Nagad) Pay(amount float64) {
-	fmt.Printf("Paying %.2f tk with Nagad", amount)
+func (ng *Nagad) Pay(amount float64) {
+	fmt.Printf("Paying %.2f tk with Nagad \n", amount)
 }
 
-func (ps PaymentService) checkout() {
-	ps.method.Pay(100.00)
+func (ps PaymentService) checkout(amount float64) {
+	ps.method.Pay(amount)
+}
+
+func NewPaymentServie(method PaymentMethod) *PaymentService {
+	return &PaymentService{method}
+}
+
+func NewBkash(apiKey string) *Bkash {
+	return &Bkash{apiKey: apiKey}
+}
+
+func NewNagad(apiKey string) *Nagad {
+	return &Nagad{apiKey: apiKey}
 }
 
 func main() {
-	// bkash := Bkash{apiKey: "1234"}
-	nagad := Nagad{apiKey: "1234"}
+	bkash := NewBkash("1234")
+	payment1 := NewPaymentServie(bkash)
+	payment1.checkout(100.00)
 
-	ps := PaymentService{
-		method: nagad,
-	}
-	ps.checkout()
+	nagad := NewNagad("1234")
+	payment2 := NewPaymentServie(nagad)
+	payment2.checkout(500.00)
+
+	mockPM := MockPaymentMethod{}
+	mockPayment := NewPaymentServie(&mockPM)
+	mockPayment.checkout(1000.00)
+}
+
+type MockPaymentMethod struct{}
+
+func (m *MockPaymentMethod) Pay(amount float64) {
+	fmt.Printf("Testing Payment Method %.2f tk....!", amount)
 }
