958636dbd131dbc3b55ae9a53bf2c0f52b68f766
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-05-12 19:09:57 +0600

    feat: module 67- Callback Functions

---
+++ module-67/Callback/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+//? Callback Function
+//? First Class Citizen =>
+
+func process(hello func()) {
+	hello()
+}
+
+func calculate(a int, b int, operation func(x, y int) int) int {
+	result := operation(a, b)
+	return result
+}
+
+func main() {
+	greet := func() {
+		fmt.Println("Hello")
+	}
+
+	process(greet)
+
+	add := func(x int, y int) int {
+		return x + y
+	}
+	multiply := func(x int, y int) int {
+		return x * y
+	}
+	fmt.Println(calculate(10, 20, add))
+	fmt.Println(calculate(10, 20, multiply))
+}
