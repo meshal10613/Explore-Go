53e7278df3c6e5e9c2fd98dcd02ef30de1fb13ac
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-05-13 10:09:23 +0600

    feat: anonymous callback function

--- module-67/Callback/main.go
+++ module-67/Callback/main.go
@@ -5,9 +5,9 @@ import "fmt"
 //? Callback Function
 //? First Class Citizen =>
 
-func process(hello func()) {
-	hello()
-}
+// func process(hello func()) {
+// 	hello()
+// }
 
 func calculate(a int, b int, operation func(x, y int) int) int {
 	result := operation(a, b)
@@ -15,18 +15,25 @@ func calculate(a int, b int, operation func(x, y int) int) int {
 }
 
 func main() {
-	greet := func() {
-		fmt.Println("Hello")
-	}
+	// greet := func() {
+	// 	fmt.Println("Hello")
+	// }
 
-	process(greet)
+	// process(greet)
 
-	add := func(x int, y int) int {
+	// add := func(x int, y int) int {
+	// 	return x + y
+	// }
+	// multiply := func(x int, y int) int {
+	// 	return x * y
+	// }
+
+	// fmt.Println(calculate(10, 20, add))
+	// fmt.Println(calculate(10, 20, multiply))
+
+	//? Anonymous Callback Function
+	callback := calculate(3, 5, func(x int, y int) int {
 		return x + y
-	}
-	multiply := func(x int, y int) int {
-		return x * y
-	}
-	fmt.Println(calculate(10, 20, add))
-	fmt.Println(calculate(10, 20, multiply))
+	})
+	fmt.Println(callback)
 }
