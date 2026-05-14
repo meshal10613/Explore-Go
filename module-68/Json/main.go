8c340cdbe606ea83259d3978a2c6e2280d546f34
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-05-14 20:17:22 +0600

    feat: more on json

--- module-68/Json/main.go
+++ module-68/Json/main.go
@@ -7,17 +7,27 @@ import (
 
 type Person struct {
 	Name string `json:"personName"` // Rename this field
-	Age  int    `json:"-"` // Exclude this field
+	Age  int    `json:"-"`          // Exclude this field
 	City string `json:"city"`
 }
 
 func main() {
 	p := Person{Name: "John", Age: 30, City: "New York"}
 	// Convert Person to JSON
-	json, err := json.Marshal(p)
+	jsonData, err := json.Marshal(p)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(string(json))
+
+	fmt.Println(string(jsonData))
+
+	var p2 Person
+	jsonText := `{"personName":"John","city":"New York"}`
+	err = json.Unmarshal([]byte(jsonText), &p2)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(p2)
 }
