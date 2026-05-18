349880678d1fef48a238014273bbb54ff15d5488
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-05-18 08:50:51 +0600

    feat: custom server mux

--- module-71/go-crud/main.go
+++ module-71/go-crud/main.go
@@ -6,15 +6,22 @@ import (
 )
 
 func main() {
+	mux := http.NewServeMux()
+
 	port := 5000
-	http.HandleFunc("/", rootHandler)
+	mux.HandleFunc("/", rootHandler)
+	mux.HandleFunc("/health", healthHandler)
 	fmt.Printf("Server is running at http://localhost:%d", port)
-	err := http.ListenAndServe(":5000", nil)
+	err := http.ListenAndServe(":5000", mux)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(mux)
 	}
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Welcome to go server....")
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Server is healthy....")
 }
